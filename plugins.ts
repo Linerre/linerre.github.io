d75b1f85c59b0cff3124c433f22746e9765523e1
Linerre <leon.errelin@outlook.com>
2025-09-01 15:38:20 +1000

    Add more markdown plugins and basic code syntax highlighting

--- plugins.ts
+++ plugins.ts
@@ -1,16 +1,17 @@
-import jsx from "lume/plugins/jsx.ts";
-import code_highlight from "lume/plugins/code_highlight.ts";
-import sitemap from "lume/plugins/sitemap.ts";
-import metas from "lume/plugins/metas.ts";
-import icons from "lume/plugins/icons.ts";
-import { merge } from "lume/core/utils/object.ts";
-import feed, { Options as FeedOptions } from "lume/plugins/feed.ts";
-import toc from "lume_markdown_plugins/toc.ts";
-import footnotes from "lume_markdown_plugins/footnotes.ts";
-import "lume/types.ts";
+import jsx from 'lume/plugins/jsx.ts';
+import codeHighlight from 'lume/plugins/code_highlight.ts';
+import sitemap from 'lume/plugins/sitemap.ts';
+import metas from 'lume/plugins/metas.ts';
+import icons from 'lume/plugins/icons.ts';
+import { merge } from 'lume/core/utils/object.ts';
+import feed, { Options as FeedOptions } from 'lume/plugins/feed.ts';
+import { attrs } from 'npm:@mdit/plugin-attrs';
+import toc from 'lume_markdown_plugins/toc.ts';
+import footnotes from 'lume_markdown_plugins/footnotes.ts';
+import 'lume/types.ts';
 
-const BASE_FONT_URL: string = "https://fonts.google.com/share?selection.family=";
-const ELECTROLIZE: string = "Electrolize";
+const BASE_FONT_URL: string = 'https://fonts.google.com/share?selection.family=';
+const ELECTROLIZE: string = 'Electrolize';
 export const defaults: Options = {
     feed: {
         output: ["/feed.xml", "/feed.json"],
@@ -36,15 +37,11 @@ export default function (userOptions?: Options) {
             .use(jsx())
             .use(toc())
             .use(footnotes())
-            .add([".css"])
-            .add("fonts")
-            .add("img")
-            .preprocess([".md"], (pages) => {
-            for (const page of pages) {
-                page.data.excerpt ??= (page.data.content as string).split(
-                    /<!--\s*more\s*-->/i,
-                )[0];
-            }
-        });
+            .use(codeHighlight())
+            .add('css')
+            .add('fonts')
+            .add('img')
+            .add(['.js'])
+            .hooks.addMarkdownItPlugin(attrs, {rule: ['block']});
     };
 }
