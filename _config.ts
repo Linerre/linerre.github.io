3286eac97dd7ca8ef70ee583c938e16fcab22621
Linerre <leon.errelin@outlook.com>
2025-09-08 00:08:39 +1000

    Correct base URL

--- _config.ts
+++ _config.ts
@@ -4,6 +4,7 @@ import plugins from "./src/plugins.ts";
 const site = lume({
     src: "./content",
     cssFile: "/css/main.css",
+    location: new URL("https://linerre.github.io/"),
     server: {
         port: 8090,
     },
