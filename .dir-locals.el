6f742a102c2583b0c948f0b0acddd4308203e210
Linerre <leon.errelin@outlook.com>
2025-09-08 17:07:22 +1000

    Add `.dir-locals.el`

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+((css-mode  . ((css-indent-offset . 2)))
+ (tsx-ts-mode . ((typescript-ts-mode-indent-offset . 2)))
+ (jsx-ts-mode . ((typescript-ts-mode-indent-offset . 2)))
+ (typescript-ts-mode . ((typescript-ts-mode-indent-offset . 2)))
+ (javascript-ts-mode . ((typescript-ts-mode-indent-offset . 2))))
